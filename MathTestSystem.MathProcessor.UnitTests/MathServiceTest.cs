bc1e9a3c161ff46cc5c8f03dc28ba7e198a1b8d4
hristinacvetanoska <hristina2401@yahoo.com>
2026-02-09 16:38:00 +0100

    added unit tests for mathprocessor

---
+++ MathTestSystem.MathProcessor.UnitTests/MathServiceTest.cs
@@ -0,0 +1,33 @@
+﻿namespace MathTestSystem.Tests
+{
+    using MathTestSystem.MathProcessor;
+    using Xunit;
+
+    public class MathServiceTests
+    {
+        private readonly IMathService mathService;
+
+        public MathServiceTests()
+        {
+            this.mathService = new MathService();
+        }
+
+        [Theory]
+        [InlineData("2+3=", 5)]
+        [InlineData("10-4=", 6)]
+        public void Evaluate_ValidFormulas_ReturnsCorrectResult(string formula, double expected)
+        {
+            var result = this.mathService.Evaluate(formula);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(5, 5, true)]
+        [InlineData(5, 4.99, false)]
+        public void CheckAnswer_CorrectAndIncorrect_ReturnsExpected(double correctResult, double studentResult, bool expected)
+        {
+            var result = this.mathService.CheckAnswer(correctResult, studentResult);
+            Assert.Equal(expected, result);
+        }
+    }
+}
